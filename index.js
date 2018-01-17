5763d3e5c81ddc4b208f69cac36efb88bd063adb
Adithi Krishnamoorthy <adithikrishnamoorthy@gmail.com>
2018-01-17 16:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function theBeatlesPlay(musicians, instruments){
+function theBeatlesPlay(musicians, instruments) {
   var array = []
   for (let i = 0, l = musicians.length; i < l; i++ ) {
     array.push(`${musicians[i]} plays ${instruments[i]}` )
@@ -16,3 +16,5 @@ function johnLennonFacts(facts) {
   }
   return array 
 }
+
+
